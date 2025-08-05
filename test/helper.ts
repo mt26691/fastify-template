99576d1a165da30c97d8279e1fd55ec9b0dec0f4
Tung Nguyen <nguyenmanhtung848@gmail.com>
2025-08-05 11:18:31 +1000

    test helper

--- test/helper.ts
+++ test/helper.ts
@@ -3,7 +3,7 @@ import { app } from '../src/app'
 import fastify from 'fastify'
 import { config } from '../src/config/env'
 
-export async function build(): Promise<FastifyInstance> {
+export const build = async (): Promise<FastifyInstance> => {
   const server = fastify({
     logger: {
       level: config.NODE_ENV === 'test' ? 'error' : 'info',
